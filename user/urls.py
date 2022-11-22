b52a403463a74847a7459c5a024f7914a5a16766
Lucas Malatchoumy <malatchoumylucas.com>
2022-11-23 00:39:25 +0100

    feat: add crud function for user model

--- user/urls.py
+++ user/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('add', views.add_user, name='add_user'),
+    path('add', views.register, name='register'),
+    path('', views.get_users, name='get_users'),
+    path('<int:user_id>', views.get_user, name='get_user'),
+    path('login', views.login, name='login'),
+    path('delete/<int:user_id>', views.delete_user , name='delete_user')
 ]
--- user/views.py
+++ user/views.py
@@ -6,33 +6,113 @@ from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_protect, csrf_exempt
 from .models import User
 from .normalizers import user_normalizer, users_normalizer
-from .forms import UserForm
+from .forms import RegisterForm, LoginForm
+from passlib.hash import pbkdf2_sha256
 
+ROLE_USER = 1
 
 # @csrf_protect
 @csrf_exempt
-def add_user(request):
+def login(request):
+    if request.method == 'POST':
+        decode = request.body.decode('utf-8')
+        content = json.loads(decode)
+        login_form = LoginForm(content)
+        if login_form.is_valid():
+            email = content['email']
+            password = content['password']
+            try:
+                user = User.objects.get(email=email)
+            except User.DoesNotExist:
+                return JsonResponse({
+                    'code': settings.HTTP_CONSTANTS['NOT_FOUND'],
+                    'result': 'error',
+                    'message': 'User not found.'
+                })
+            check_password = pbkdf2_sha256.verify(password, user.password)
+            if check_password:
+                message = 'You are logged in'
+            else:
+                return JsonResponse({
+                    'code': settings.HTTP_CONSTANTS['NOT_ALLOWED'],
+                    'result': 'Not Allowed',
+                    'message': 'password incorrect',
+                })
+        else:
+            return JsonResponse({
+                'code': settings.HTTP_CONSTANTS['INTERNAL_SERVER_ERROR'],
+                'result': 'error',
+                'message': 'Could not save the data',
+                'data': login_form.errors
+            })
+    else:
+        return JsonResponse({
+            'code': settings.HTTP_CONSTANTS['NOT_ALLOWED'],
+            'result': 'Not Allowed',
+            'message': 'Must be a POST method',
+        })
+    return JsonResponse({'code': settings.HTTP_CONSTANTS['SUCCESS'], 'result': 'success', 'message': message})
+
+@csrf_exempt
+def get_users(request):
+    if request.method == 'GET':
+        users = User.objects.all().values()
+        if users:
+            data = users_normalizer(users)
+        else:
+            return JsonResponse({'code': settings.HTTP_CONSTANTS['SUCCESS'], 'result': 'success', 'data': []})
+    else:
+        return JsonResponse({
+            'code': settings.HTTP_CONSTANTS['NOT_ALLOWED'],
+            'result': 'Not Allowed',
+            'message': 'Must be a GET method',
+        })
+
+    return JsonResponse({'code': settings.HTTP_CONSTANTS['SUCCESS'], 'result': 'success', 'data': data})
+
+@csrf_exempt
+def get_user(request, user_id):
+    if request.method == "GET":
+        try:
+            user = User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return JsonResponse({
+                'code': settings.HTTP_CONSTANTS['NOT_FOUND'],
+                'result': 'error',
+                'message': 'User not found.'
+            })
+
+        data = user_normalizer(user)
+    else:
+        return JsonResponse({
+            'code': settings.HTTP_CONSTANTS['NOT_ALLOWED'],
+            'result': 'Not Allowed',
+            'message': 'Must be a GET method',
+        })
+
+    return JsonResponse({'code': settings.HTTP_CONSTANTS['SUCCESS'], 'result': 'success', 'data': data})
+
+@csrf_exempt
+def register(request):
     if request.method == "POST":
         decode = request.body.decode('utf-8')
         content = json.loads(decode)
-        form = UserForm(content)
+        form = RegisterForm(content)
 
         if form.is_valid():
-            form.save()
-            data = user_normalizer(User.objects.latest('id'))
-
-            # if content['role']:
-            #     try:
-            #         santa = Role.objects.get(pk=content['role'])
-            #     except Role.DoesNotExist:
-            #         return JsonResponse({
-            #             'code': settings.HTTP_CONSTANTS['NOT_FOUND'],
-            #             'result': 'error',
-            #             'message': 'Role not found.'
-            #         })
-            #
-            #     roleOrganization = RoleOrganization(organization=Organization.objects.latest('id'), role=role)
-            #     RoleOrganization.save(roleOrganization)
+            email = content['email']
+            if not User.objects.filter(email=email).exists():
+                new_user = form.save(commit=False)
+                new_user.password = pbkdf2_sha256.hash(new_user.password)
+                new_user.role = ROLE_USER
+                new_user.save()
+                data = user_normalizer(User.objects.latest('id'))
+            else:
+                return JsonResponse({
+                    'code': settings.HTTP_CONSTANTS['NOT_ALLOWED'],
+                    'result': 'Not Allowed',
+                    'message': 'This user account already exists',
+                })      
         else:
             return JsonResponse({
                 'code': settings.HTTP_CONSTANTS['INTERNAL_SERVER_ERROR'],
@@ -49,3 +129,24 @@ def add_user(request):
 
     return JsonResponse({'code': settings.HTTP_CONSTANTS['CREATED'], 'result': 'success', 'data': data})
 
+@csrf_exempt
+def delete_user(request, user_id):
+    if request.method == 'DELETE':
+        try:
+            user = User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return JsonResponse({
+                'code': settings.HTTP_CONSTANTS['NOT_FOUND'],
+                'result': 'error',
+                'message': 'User not found.'
+            })
+
+        user.delete()
+    else:
+        return JsonResponse({
+            'code': settings.HTTP_CONSTANTS['NOT_ALLOWED'],
+            'result': 'Not Allowed',
+            'message': 'Must be a DELETE method',
+        })
+
+    return JsonResponse({'code': settings.HTTP_CONSTANTS['SUCCESS'], 'result': 'success', 'data': []})
