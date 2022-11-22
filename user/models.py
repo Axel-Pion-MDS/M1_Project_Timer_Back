1ccf4958be9b351d84764358edb425599ebc6fff
Lucas Malatchoumy <malatchoumylucas.com>
2022-11-23 00:37:30 +0100

    feat: add role field for user model

--- user/models.py
+++ user/models.py
@@ -8,6 +8,8 @@ class User(models.Model):
     password = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
+    role = models.IntegerField()
+    # role = models.ForeignKey('role.role', on_delete=models.CASCADE, null=False)
 
     def __str__(self):
         return '{}. {} {} - with the email: {}, created on {}, updated on {}'.format(self.pk, self.firstname, self.lastname, self.email, self.created_at, self.updated_at)
