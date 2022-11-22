93c8722437e32ac801dd4df2c575ae8978d561c1
Lucas Malatchoumy <malatchoumylucas.com>
2022-11-23 00:38:19 +0100

    feat: create login form

--- user/forms.py
+++ user/forms.py
@@ -2,12 +2,22 @@ from django import forms
 from .models import User
 
 
-class UserForm(forms.ModelForm):
+class RegisterForm(forms.ModelForm):
     class Meta:
         model = User
         fields = (
             'firstname',
             'lastname',
+            'email',
+            'password'
+        )
+        widgets = {
+            'password': forms.PasswordInput(),
+        }
+class LoginForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = (
             'email',
             'password'
         )
