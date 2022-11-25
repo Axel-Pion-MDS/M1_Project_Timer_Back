95c43e9abd33c50cbcc3b9f83a826fe8b7a42ce5
Axel Pion <axel.pion@fr-pan-2277.home>
2022-11-25 19:36:16 +0100

    refactor: correct routes

--- organization/urls.py
+++ organization/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.get_organizations, name='get_organizations'),
-    path('/<int:organization_id>', views.get_organization, name='get_organization'),
-    path('/add', views.add_organization, name='add_organization'),
-    path('/update', views.update_organization, name='update_organization'),
-    path('/delete/<int:organizationx_id>', views.delete_organization, name='delete_organization'),
+    path('<int:organization_id>', views.get_organization, name='get_organization'),
+    path('add', views.add_organization, name='add_organization'),
+    path('update', views.update_organization, name='update_organization'),
+    path('delete/<int:organizationx_id>', views.delete_organization, name='delete_organization'),
 ]
