9486d6fc3d259e9aeaeb6d8479fc3f6edf6d99bc
Lucas Malatchoumy <malatchoumylucas.com>
2022-11-26 19:34:08 +0100

    fixup! feat: add crud function for user model

--- user/models.py
+++ user/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class User(models.Model):
     firstname = models.CharField(max_length=100)
     lastname = models.CharField(max_length=100)
-    email = models.EmailField(max_length=254)
+    email = models.EmailField(max_length=255)
     password = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
