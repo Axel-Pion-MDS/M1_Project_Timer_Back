398466666a0c8fbbbbc5906c9caec7dc05cdb3f0
Axel Pion <axel.pion@fr-pan-2277.home>
2022-11-26 13:51:05 +0100

    feat: add crud for role, also add role constants

--- app/settings.py
+++ app/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'role.apps.RoleConfig',
     'organization.apps.OrganizationConfig',
     'role.apps.RoleConfig',
     'user.apps.UserConfig'
