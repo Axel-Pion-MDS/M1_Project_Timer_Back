909fb18793d670aabf7f500f85f1834ef6b4372e
Lucas Malatchoumy <malatchoumylucas.com>
2022-11-27 16:07:46 +0100

    feat: implement foreign key role for user

--- user/models.py
+++ user/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class User(models.Model):
@@ -8,8 +9,7 @@ class User(models.Model):
     password = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
-    role = models.IntegerField()
-    # role = models.ForeignKey('role.role', on_delete=models.CASCADE, null=False, **default=settings.ROLE_CONSTANTS['ROLE_USER']**)
+    role = models.ForeignKey('role.Role', on_delete=models.CASCADE, null=False, default=settings.ROLES['ROLE_USER'])
 
     def __str__(self):
         return '{}. {} {} - with the email: {}, created on {}, updated on {}'.format(self.pk, self.firstname, self.lastname, self.email, self.created_at, self.updated_at)
--- user/normalizers.py
+++ user/normalizers.py
@@ -4,6 +4,7 @@ from .models import User
 def users_normalizer(data):
     result = []
     for user in data:
+        user_details = User.objects.get(pk=user['id'])
 
         item = {
             'id': user['id'],
@@ -12,6 +13,10 @@ def users_normalizer(data):
             'email': user['email'],
             'created_at': user['created_at'],
             'updated_at': user['updated_at'],
+            'role': {
+                'id': user_details.role.id,
+                'label': user_details.role.label
+            } if user_details.role else 'null'
         }
 
         result.append(item)
@@ -27,4 +32,8 @@ def user_normalizer(data):
         'email': data.email,
         'created_at': data.created_at,
         'updated_at': data.updated_at,
+        'role': {
+                'id': data.role.id,
+                'label': data.role.label
+        } if data.role else 'null'
     }
