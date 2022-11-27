a9906e6a4bafdb4bf855a1a6703fb29485594d06
Maengdok <pionaxel@gmail.com>
2022-11-27 17:29:49 +0100

    merge conflict

--- app/settings.py
+++ app/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'organization.apps.OrganizationConfig',
     'role.apps.RoleConfig',
     'organization.apps.OrganizationConfig',
     'task.apps.TaskConfig',
