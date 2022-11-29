33a64741b49929a47bf5b89dde0a8c30c6ebbfc3
sylvaindnd <62701085+sylvaindnd@users.noreply.github.com>
2022-11-29 13:35:30 +0100

    feat: update user inside a team

--- team/urls.py
+++ team/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('add', views.add_team, name='add_team'),
     path('update', views.update_team, name='update_team'),
     path('delete/<int:team_id>', views.delete_team, name='delete_team'),
-    path('users/add', views.add_user_team, name='add_user_team')
+    path('users/add', views.add_user_team, name='add_user_team'),
+    path('users/update', views.update_user_team, name='update_user_team'),
 ]
