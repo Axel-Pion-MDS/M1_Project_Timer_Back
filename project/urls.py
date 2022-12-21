ac50046ce95817c52f29d427945286fc1f5fbd79
Lucas Malatchoumy <malatchoumylucas.com>
2022-12-21 22:35:41 +0100

    feat: create project routes

--- project/urls.py
+++ project/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.get_projects, name='get_projects'),
-    # path('<int:project_id>', views.get_project, name='get_project'),
+    path('', views.get_projects, name='get_projects'),
+    path('<int:project_id>', views.get_project, name='get_project'),
     path('add', views.add_project, name='add_project'),
-    # path('update', views.update_project, name='update_project'),
-    # path('delete/<int:project_id>', views.delete_project, name='delete_project'),
+    path('update', views.update_project, name='update_project'),
+    path('delete/<int:project_id>', views.delete_project, name='delete_project'),
 ]
