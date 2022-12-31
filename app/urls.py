2be463325b3604ad994acd8df24581b1891340c6
Maengdok <pionaxel@gmail.com>
2022-12-31 08:49:10 +0100

    refactor: redirect path '/' to 'api-doc/'

--- app/urls.py
+++ app/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include, re_path
 from app import views
 
@@ -29,4 +30,5 @@ urlpatterns = [
     path('user-task/', include('user_task.urls')),
     path('token/', views.send_csrf_token, name="send_csrf_token"),
     path('', include('swagger_ui.urls')),
+    path('', lambda request: redirect('api-doc/', permanent=True)),
 ]
