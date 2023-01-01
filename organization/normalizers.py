000ee9087ffdcf11c82b549ca0a1657c40d22514
Maengdok <pionaxel@gmail.com>
2023-01-01 13:30:03 +0100

    refactor: add teams in organization's normalizers

--- organization/normalizers.py
+++ organization/normalizers.py
@@ -1,10 +1,13 @@
 from project.models import Project
+from team.models import Team
 
 
 def organizations_normalizer(data):
     result = []
     for organization in data:
         project_details = Project.objects.filter(organization=organization['id']).all()
+        team_details = Team.objects.filter(organization=organization['id']).all()
+
         projects = []
         for project in project_details:
             item = {
@@ -14,12 +17,22 @@ def organizations_normalizer(data):
 
             projects.append(item)
 
+        teams = []
+        for team in team_details:
+            item = {
+                'id': team.id,
+                'label': team.label
+            }
+
+            teams.append(item)
+
         item = {
             'id': organization['id'],
             'label': organization['label'],
             'description': organization['description'],
             'created_at': organization['created_at'],
             'updated_at': organization['updated_at'],
+            'teams': teams if teams else 'null',
             'projects': projects if projects else 'null'
         }
 
@@ -30,6 +43,8 @@ def organizations_normalizer(data):
 
 def organization_normalizer(data):
     project_details = Project.objects.filter(organization=data.id).all()
+    team_details = Team.objects.filter(organization=data.id).all()
+
     projects = []
     for project in project_details:
         item = {
@@ -39,11 +54,21 @@ def organization_normalizer(data):
 
         projects.append(item)
 
+    teams = []
+    for team in team_details:
+        item = {
+            'id': team.id,
+            'label': team.label
+        }
+
+        teams.append(item)
+
     return {
         'id': data.id,
         'label': data.label,
         'description': data.description,
         'created_at': data.created_at,
         'updated_at': data.updated_at,
+        'teams': teams if teams else 'null',
         'projects': projects if projects else 'null'
     }
